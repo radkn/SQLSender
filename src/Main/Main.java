14a0d693bc36ba47832892aff7163bb526850617
radkn <radkn@users.noreply.github.com>
2017-11-16 00:22:48 +0200

    DB_SELECT_ToJSON - метод возвращает JSON строку с таблицы Line

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,30 +3,48 @@ package Main;
 import java.sql.*;
 
 public class Main {
-    public static String DB_NAME = "jdbc:mysql://localhost:3306/Students";
-    public  static String DB_USER = "root";
-    public  static String DB_PASSWORD = "root";
+    private static String DB_NAME = "jdbc:mysql://localhost:3306/NewVision";
+    private static String DB_USER = "root";
+    private static String DB_PASSWORD = "root";
 
-    public static void main(String[] args) throws ClassNotFoundException {
-        DB_SELECT();
+    public static void main(String[] args){
+        System.out.println(DB_SELECT_ToJSON());
     }
 
-    public static void DB_SELECT(){
-        String query = "SELECT * FROM Students";
+    public static String DB_SELECT_ToJSON(){
+        String query = "SELECT * FROM Line";
         PreparedStatement statement = null;
         Connection connection = null;
         ResultSet resultSet = null;
+        String allObject = "[\n";
+        String lineTitle;
+        String uid;
+        String dataTime;
+        String status;
+        String type;
+
         try {
             connection = DriverManager.getConnection(DB_NAME, DB_USER, DB_PASSWORD);
             statement = connection.prepareStatement(query);
             resultSet = statement.executeQuery();
-            System.out.println("id" + "    " + "name" + "    " + "mark");
             while(resultSet.next()) {
-                int id = resultSet.getInt(1);
-                String Name = resultSet.getString(2);
-                double Mark = resultSet.getDouble(3);
-                System.out.println(id + "    " + Name + "    " + Mark);
+
+                lineTitle = resultSet.getString(3);
+                uid = resultSet.getString(4);
+                dataTime = resultSet.getString(5);
+                dataTime = dataTime.substring(0, dataTime.length()-2);// обрезка двух лишних символов с базы (".0")
+                status = resultSet.getString(6);
+                type = resultSet.getString(7);
+
+                allObject+=  "\t{\n\t\t\"lineTitle\":\"" + lineTitle
+                        + "\",\n\t\t\"uid\":\"" + uid
+                        + "\",\n\t\t\"dataTime\":\"" + dataTime
+                        + "\",\n\t\t\"status\":\"" + status
+                        + "\",\n\t\t\"type\":\"" + type
+                        + "\"\n\t},\n";
             }
+            allObject = allObject.substring(0,allObject.length()-2);
+            allObject+="\n]";
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -46,7 +64,7 @@ public class Main {
                 e.printStackTrace();
             }
         }
-
+        return allObject;
     }
 
 }
