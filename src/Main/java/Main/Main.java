514da3d7dc083a21cb32457954ec778ca470c34b
JulyaDon <don.julya@yandex.ru>
2017-12-14 17:55:07 +0200

    timer added

--- src/Main/java/Main/Main.java
+++ src/Main/java/Main/Main.java
@@ -6,13 +6,12 @@ import java.util.Scanner;
 import javax.swing.Timer;
 
 public class Main {
+
     public static void main(String[] args){
-        Timer timerSendData;
         String parametersAddress = "parameters/parameters.xml";
         XMLwriterReader<Parameters> reader = new XMLwriterReader(parametersAddress);
         //here we get our parameters from .xml file
         Parameters param = reader.ReadFile(Parameters.class);
-
 
         //here we check whether we need to send data to reserve DB
         //if true, we send
@@ -33,39 +32,45 @@ public class Main {
         }
         System.out.println("Count L: " + NVToServer.getCountOfZones());
 
+
+        Timer timerSendData = new Timer(param.getCheckTransmittedPeriod(), new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                boolean sendSuccess;
+                //here we send new Lines data to server
+                long countL = NVToServer.getCountOfLines(); //records with transmitted=false
+                while (countL > 0) {
+                    countL = NVToServer.getCountOfLines();
+                    if (countL >= param.getOnePackOfStrings())
+                        sendSuccess = NVToServer.sendLines(param.getOnePackOfStrings());
+                    else
+                        sendSuccess = NVToServer.sendLines(countL);
+                    System.out.println("Lines success: " + sendSuccess);
+                }
+
+                //here we send new Zones data to server
+                long countZ = NVToServer.getCountOfZones(); //records with transmitted=false
+                while (countZ > 0) {
+                    countZ = NVToServer.getCountOfZones();
+                    if (countZ >= param.getOnePackOfStrings())
+                        sendSuccess = NVToServer.sendZones(param.getOnePackOfStrings());
+                    else
+                        sendSuccess = NVToServer.sendZones(countZ);
+                    System.out.println("Zones success: " + sendSuccess);
+                }
+            }
+        });
+        timerSendData.start();
+
         //program will never stop
         while (true) {
-            timerSendData = new Timer(param.getCheckTransmittedPeriod(), new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    boolean sendSuccess;
-
-
-                    //here we send new Lines data to server
-                    long countL = NVToServer.getCountOfLines(); //records with transmitted=false
-                    while (countL > 0) {
-                        countL = NVToServer.getCountOfLines();
-                        if (countL >= param.getOnePackOfStrings())
-                            sendSuccess = NVToServer.sendLines(param.getOnePackOfStrings());
-                        else
-                            sendSuccess = NVToServer.sendLines(countL);
-                        System.out.println("Lines success: " + sendSuccess);
-                    }
-
-                    //here we send new Zones data to server
-                    long countZ = NVToServer.getCountOfZones(); //records with transmitted=false
-                    while (countZ > 0) {
-                        countZ = NVToServer.getCountOfZones();
-                        if (countZ >= param.getOnePackOfStrings())
-                            sendSuccess = NVToServer.sendZones(param.getOnePackOfStrings());
-                        else
-                            sendSuccess = NVToServer.sendZones(countZ);
-                        System.out.println("Zones success: " + sendSuccess);
-                    }
-                }
-            });
-
-//
+            Scanner sc = new Scanner(System.in);
+            System.out.println("If you want to stop the program enter 'stop'");
+            String stop = sc.nextLine();
+            if(stop.equals("stop")){
+                timerSendData.stop();
+                break;
+            }
         }
     }
 }
